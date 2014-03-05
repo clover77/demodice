dc68c7015297632c6ce34140ea8308c62e4a5c73
Justin <jfong@lalala.local>
2014-03-05 23:20:05 +0800

    Initial Commit

---
+++ demodice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  demodice
+//
+//  Created by Justin on 5/3/14.
+//  Copyright (c) 2014 Justin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
